019e5f05eb429f67d01d141df4404122ec616308
Evgen Savinov <evs@sitecore.net>
2018-02-22 12:44:45 +0200

    initial commit

---
+++ src/Sitecore.Support.165940/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.165940")]
+[assembly: AssemblyProduct("Sitecore.Support.165940")]
+[assembly: ComVisible(false)]
